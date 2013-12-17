35faca7c0fb0cf0acb7ba9203b96f11b25fbdeaf
feng dejiang <fengdj521@gmail.com>
2013-12-18 00:55:31 +0900

    Test Commit

--- src/name/leesah/iki/gae/UidServlet.java
+++ src/name/leesah/iki/gae/UidServlet.java
@@ -2,10 +2,6 @@ package name.leesah.iki.gae;
 
 import java.io.IOException;
 
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
 /**
  * @author Sah
  * 
